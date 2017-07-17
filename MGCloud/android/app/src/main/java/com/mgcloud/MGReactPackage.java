ffa9218647798c8346f243c696ea5e739f96bb92
liangjinzhu <liangjinzhu@haima.me>
2017-07-17 16:56:05 +0800

    [功能开发]删除无用的module，调整依赖项目。
    Change-Id: I2f0dc41d6e18af6ce427f4db596618b87be03a73

--- MGCloud/android/app/src/main/java/com/mgcloud/MGReactPackage.java
+++ MGCloud/android/app/src/main/java/com/mgcloud/MGReactPackage.java
@@ -5,12 +5,14 @@ import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 /**
  * 注册jsmodule组件
+ *
  * @author shisheng.zhao
  * @date 2017-06-26
  */
@@ -25,6 +27,7 @@ public class MGReactPackage implements ReactPackage {
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         List<NativeModule> modules = new ArrayList<>();
         modules.add(new RNInteration(reactContext));
+        modules.add(new OrientationModule(reactContext));
         return modules;
     }
 
--- MGCloud/android/app/src/main/java/com/mgcloud/MainApplication.java
+++ MGCloud/android/app/src/main/java/com/mgcloud/MainApplication.java
@@ -1,15 +1,16 @@
 package com.mgcloud;
 
 import android.app.Application;
+
 import com.facebook.react.ReactApplication;
-import com.github.yamill.orientation.OrientationPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+
 import java.util.Arrays;
 import java.util.List;
-import com.github.yamill.orientation.OrientationPackage;
+
 public class MainApplication extends Application implements ReactApplication {
 
     private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
@@ -22,8 +23,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
                     new MainReactPackage(),
-                    new MGReactPackage(),
-                    new OrientationPackage()
+                    new MGReactPackage()
             );
         }
     };
